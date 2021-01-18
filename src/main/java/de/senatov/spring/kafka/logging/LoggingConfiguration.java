a3bab126871da45c765e49d2cc0fd95471aa25e6
Iakov Senatov <javaEntwickler@gmail.com>
2021-01-18 16:08:39 +0100

    some improvements

--- src/main/java/de/senatov/spring/kafka/logging/LoggingConfiguration.java
+++ src/main/java/de/senatov/spring/kafka/logging/LoggingConfiguration.java
@@ -1,11 +1,14 @@
 package de.senatov.spring.kafka.logging;
 
-import java.lang.reflect.Member;
+
+
 import org.slf4j.Logger;
 import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+
+import java.lang.reflect.Member;
 
 import static java.lang.System.err;
 import static java.util.Optional.of;
@@ -19,24 +22,27 @@ import static org.slf4j.LoggerFactory.getLogger;
 @Configuration
 public class LoggingConfiguration {
 
-  private static final String OCCURED_S_N = "slf4j autowired Exception occured : %s%n";
+    private static final String OCCURED_S_N = "slf4j autowired Exception occured : %s%n";
 
-  /**
-   * Log logger.
-   *
-   * @param ip the ip
-   * @return the logger
-   */
-  @Bean
-  @Scope("prototype")
-  public Logger log(InjectionPoint ip) {
 
-    try {
-      return getLogger(of(ip.getMember()).map(Member::getDeclaringClass).orElseThrow(IllegalArgumentException::new));
-    } catch (Exception e) {
-      err.printf(OCCURED_S_N, e.getMessage());
-      throw e;
+
+    /**
+     * Log logger.
+     *
+     * @param ip the ip
+     * @return the logger
+     */
+    @Bean
+    @Scope("prototype")
+    public Logger log(InjectionPoint ip) {
+
+        try {
+            return getLogger(of(ip.getMember()).map(Member::getDeclaringClass).orElseThrow(IllegalArgumentException::new));
+        } catch (Exception e) {
+            err.printf(OCCURED_S_N, e.getMessage());
+            throw e;
+        }
     }
-  }
+
 }
 
--- src/main/java/de/senatov/spring/kafka/pandcdemo/PAndCDemoApplication.java
+++ src/main/java/de/senatov/spring/kafka/pandcdemo/PAndCDemoApplication.java
@@ -1,13 +1,18 @@
 package de.senatov.spring.kafka.pandcdemo;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class PAndCDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PAndCDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(PAndCDemoApplication.class, args);
+    }
 
 }
