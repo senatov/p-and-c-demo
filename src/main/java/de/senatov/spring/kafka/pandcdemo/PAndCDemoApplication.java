f42e71eb014e6e91c8c8ea06830810aa851d1dbb
Iakov Senatov <javaEntwickler@gmail.com>
2021-01-18 04:19:50 +0100

    initial

---
+++ src/main/java/de/senatov/spring/kafka/pandcdemo/PAndCDemoApplication.java
@@ -0,0 +1,13 @@
+package de.senatov.spring.kafka.pandcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PAndCDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PAndCDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/senatov/spring/kafka/pandcdemo/PAndCDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.senatov.spring.kafka.pandcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PAndCDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
