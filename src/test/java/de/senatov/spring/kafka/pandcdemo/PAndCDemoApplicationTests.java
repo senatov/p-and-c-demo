a91549913992fead281acf8add3f53fee9995598
Iakov Senatov <javaEntwickler@gmail.com>
2021-01-18 22:11:35 +0100

    - de.senatov.spring.kafka.logging - NLP Err

--- src/test/java/de/senatov/spring/kafka/pandcdemo/PAndCDemoApplicationTests.java
+++ src/test/java/de/senatov/spring/kafka/pandcdemo/PAndCDemoApplicationTests.java
@@ -1,13 +1,30 @@
 package de.senatov.spring.kafka.pandcdemo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+
+
+
 class PAndCDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ApplicationArguments args;
+    @Autowired
+    private Logger log;
+
+
+
+    /**
+     * Context loads.
+     */
+    @Test
+    public void contextLoads() {
+
+        log.debug("Hallo");
+    }
 
 }
