9d4de3cca58d41ce8023dfc08872d13fea51bf93
Iakov Senatov <javaEntwickler@gmail.com>
2021-01-19 13:29:58 +0100

    -works

--- src/test/java/de/senatov/spring/kafka/pandcdemo/test/PAndCDemoApplicationTests.java
+++ src/test/java/de/senatov/spring/kafka/pandcdemo/test/PAndCDemoApplicationTests.java
@@ -1,30 +1,30 @@
-package de.senatov.spring.kafka.pandcdemo;
+package de.senatov.spring.kafka.pandcdemo.test;
 
 
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
 
 
 
 class PAndCDemoApplicationTests {
 
     @Autowired
-    private ApplicationArguments args;
-    @Autowired
-    private Logger log;
+    Logger LOG;
 
 
 
     /**
      * Context loads.
      */
+    //private static final Logger LOG = LoggerFactory.getLogger(PAndCDemoApplicationTests.class);
     @Test
+
     public void contextLoads() {
 
-        log.debug("Hallo");
+        LOG.debug("Tests...");
+
     }
 
 }
